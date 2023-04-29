682ed1fd9c2fc457617afc323d57e1fbadab8a39
long.dlb <60988318+JohnnyDiep1021@users.noreply.github.com>
2023-04-29 11:52:26 -0400

    Feature - Create and get role

---
+++ src/controller/role.controller.js
@@ -0,0 +1,30 @@
+const service = require("../services/role.service");
+const { validationResult } = require("express-validator");
+const HttpError = require("../utils/error");
+
+const addRole = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) throw new HttpError(`Invalid inputs passed!`, 422);
+    const newRole = await service.addRole(req);
+    res.status(201).json({
+      role: newRole,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getRoleById = async (req, res, next) => {
+  try {
+    if (!req.params.id) throw new HttpError(`Require roleId!`, 422);
+    const newRole = await service.getRoleById(req);
+    res.status(200).json({
+      role: newRole,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { addRole, getRoleById };
--- src/routes/root.router.js
+++ src/routes/root.router.js
@@ -1,9 +1,12 @@
 const express = require("express");
 // const { propertyRoute } = require("./porperty.route");
 // const { userRouter } = require("./user.router");
+const { roleRouter } = require("./role.router");
 
 const rootRouter = express.Router();
 
 // rootRouter.use("/user", userRouter);
 // rootRouter.use("/property", propertyRoute);
+rootRouter.use("/role", roleRouter);
+
 module.exports = { rootRouter };
