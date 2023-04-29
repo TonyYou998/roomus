39baaf10abaec7ecdccb6e4ca20c5d3412859401
long.dlb <60988318+JohnnyDiep1021@users.noreply.github.com>
2023-04-29 10:41:19 -0400

    Add login

--- src/routes/root.router.js
+++ src/routes/root.router.js
@@ -1,9 +1,9 @@
 const express = require("express");
 // const { propertyRoute } = require("./porperty.route");
-// const { userRouter } = require("./user.router");
+const { userRouter } = require("./user.router");
 
 const rootRouter = express.Router();
 
-// rootRouter.use("/user", userRouter);
+rootRouter.use("/user", userRouter);
 // rootRouter.use("/property", propertyRoute);
 module.exports = { rootRouter };
