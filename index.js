6bb81bcde62d8caf2080c80a1809bd836e496c56
tony-you-sout <tanvuu998@gmail.com>
2023-05-01 23:20:31 +0700

    push

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const { sequelize } = require("./src/models");
 const { rootRouter } = require("./src/routes/root.router");
 
 const cors = require("cors");
-app.use(cors(config.get("cors")));
+//app.use(cors(config.get("cors")));
+app.use(cors());
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
