9bae672c91627af2dce129fd648b3983c6c69c98
Diep, Ly Bao Long <60988318+JohnnyDiep1021@users.noreply.github.com>
2023-05-03 15:23:58 +0700

    Feature_create-business

--- src/routes/root.router.js
+++ src/routes/root.router.js
@@ -3,17 +3,17 @@ const express = require("express");
 // const { userRouter } = require("./user.router");
 const { serviceRouter } = require("./service.router");
 
-
 const { roleRouter } = require("./role.router");
 const { userRouter } = require("./user.router");
+const { businessRouter } = require("./business.router");
 
 const rootRouter = express.Router();
-rootRouter.use("/service",serviceRouter);
+rootRouter.use("/service", serviceRouter);
 
 rootRouter.use("/user", userRouter);
+rootRouter.use("/business", businessRouter);
 
 rootRouter.use("/role", roleRouter);
-
 
 rootRouter.use("/service", serviceRouter);
 
