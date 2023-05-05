1b857b958133c5f9dd7b72d3bb2557cd7dc72e95
tony-you-sout <tanvuu998@gmail.com>
2023-05-05 22:04:01 +0700

    dasdsad

--- src/services/service.service.js
+++ src/services/service.service.js
@@ -187,40 +187,8 @@ const getDetailItemById = async (id) => {
   }
 };
 
-const addServiceItem=async (request)=>{
-    const {serviceId,images,price,description,itemType,serviceItemName}=request;
-    try {
-        const newServiceItem=await ServiceItem.create({
-            id:uuidv4(),
-            serviceId,
-            images,
-            status:"EMPTY",
-            price,
-            description,
-            itemType,
-            serviceItemName,
-        });
-        return newServiceItem;
-    } catch (error) {
-        throw error;
-    }
-    
 
 
-}
-const getDetailItemById=async (id)=>{
-    try {
-        const item=await ServiceItem.findOne({
-            where:{
-                id,
-            }
-        });
-        return item;
-    } catch (error) {
-        throw error;
-    }
-
-}
 const getServiceByServiceTypeId=async (serviceTypeId)=>{
   try {
    
